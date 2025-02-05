125f73b43c70347f25ae3333eb20b52a78a67415
shuraimnms <shuraimnms@gmail.com>
2025-02-05 12:52:56 +0530

    Add files via upload

---
+++ portfolio/portfolio/script/app.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    // Animate the progress bars
+    document.querySelectorAll(".progress-bar").forEach(bar => {
+        const progress = bar.getAttribute("data-progress");
+        bar.style.width = progress;
+    });
+})
