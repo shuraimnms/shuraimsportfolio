9a438af8a5c760b8e51443691a2a09de8b72cef0
shuraimnms <shuraimnms@gmail.com>
2025-02-05 07:13:00 +0000

    up

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// Scroll-triggered animation (for skill bars)
-window.addEventListener('scroll', function() {
-    const skillSections = document.querySelectorAll('.progress-bar');
-    skillSections.forEach((skill) => {
-        const rect = skill.getBoundingClientRect();
-        if (rect.top < window.innerHeight) {
-            skill.style.width = skill.getAttribute('class').includes('html') ? '90%' : '75%';
-        }
+window.addEventListener('load', () => {
+    // Animate the progress bars
+    document.querySelectorAll(".progress-bar").forEach(bar => {
+        const progress = bar.getAttribute("data-progress");
+        bar.style.width = progress;
     });
-});
+})
