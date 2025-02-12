100ababd5633408d0f0081e4bc6890fe8cda8efc
shuraimnms <shuraimnms@gmail.com>
2025-02-12 12:27:41 +0530

    Delete portfolio directory

--- portfolio/scripts/app.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-    // Animate the progress bars
-    document.querySelectorAll(".progress-bar").forEach(bar => {
-        const progress = bar.getAttribute("data-progress");
-        bar.style.width = progress;
-    });
-});
